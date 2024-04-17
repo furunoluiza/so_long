1ed71a88df468d72d58464799bdef9e49a4bfac1
lfuruno- <luizafuruno@gmail.com>
2024-04-17 18:54:39 -0300

    Problema de segfaut resolvido

--- src/valid_playable.c
+++ src/valid_playable.c
@@ -14,30 +14,21 @@
 
 static void fill_map(t_map *copy_map, int x, int y)
 {
-    //printf("%c", copy_map->map[x][y]);
-    int i = 0;
-    while (copy_map->map[i])
-    {
-        printf("-> %s\n", copy_map->map[i]);
-        //printf("len: %li\n", ft_strlen(copy_map->map[i]));
-        i++;
-        printf("i = %d", i);
-    }
     if (x == copy_map->x_player && y == copy_map->y_player)
         printf("No começo.\n");
     else
         printf("X: %d, Y: %d.\n", x, y);
-    if (copy_map->map[x][y] == 'C')
+    if (copy_map->map[y][x] == 'C')
        copy_map->verify_collectible--;
-    if (copy_map->map[x][y] == 'E')
+    if (copy_map->map[y][x] == 'E')
     {
         copy_map->verify_exit = 1;
-        copy_map->map[x][y] = '1';
+        copy_map->map[y][x] = '1';
     }
     if (x < 1 || x > copy_map->width || y < 1 || y > copy_map->height
-        || copy_map->map[x][y] == '1')
+        || copy_map->map[y][x] == '1')
         return ;
-    copy_map->map[x][y] = '1';
+    copy_map->map[y][x] = '1';
     fill_map(copy_map, x - 1, y);
     fill_map(copy_map, x + 1, y);
     fill_map(copy_map, x, y - 1);
@@ -47,8 +38,16 @@ static void fill_map(t_map *copy_map, int x, int y)
 int playable_map(char **map)
 {
     t_map   *copy_map;
+    int i = 0;
 
     copy_map = struct_map(map);
+    while (copy_map->map[i])
+    {
+        //printf("i = %d\n", i);
+        printf("-> %s", copy_map->map[i]);
+        //printf("len: %li\n", ft_strlen(copy_map->map[i]));
+        i++;
+    }
     fill_map(copy_map, copy_map->x_player, copy_map->y_player);
     if (copy_map->verify_collectible != 0 || copy_map->verify_exit == 0)
         return(PLAYABLE_ERROR);
